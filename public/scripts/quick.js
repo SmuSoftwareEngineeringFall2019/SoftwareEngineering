5ef58774febb2f717d042b5a1f8d8de773888056
BigCheddar2 <48729895+BigCheddar2@users.noreply.github.com>
2019-11-14 14:22:20 -0400

    Update quick.js

--- public/scripts/quick.js
+++ public/scripts/quick.js
@@ -1,38 +1,27 @@
-/** 
- * TODO 
- * Change from local storage
- * end of line and new characterto go on next line
- * cursor position after undo/redo.
- * JUDY WITH ONLY TOP BUTTONS TO WORK WITH KEYBOARD
- * http://ugdev.cs.smu.ca:3000/create
- * 
- * 
- * Author: Jacob Vincent (A00419169)
- **/
-var lowerBoard = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=",
-    "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]",
-    "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "\\'", "\\\\",
-    "z", "x", "c", "v", "b", "n", "m", ",", ".", "/"];
-
-var capBoard = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+",
-    "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}",
-    "A", "S", "D", "F", "G", "H", "J", "K", "L", ":", "&quot;", "|",
-    "Z", "X", "C", "V", "B", "N", "M", "<", ">", "?"];
-
-//Stacks for redoing/undoing
 var oldStack = [];
 var currentStack;
 
 //current text field being edited
-var focus = "title";
-
-//capital or lowercase keyboard
-var cap = false;
+var focus = "userText";
 
 //Variables for cursor and it's position in the text
 var curs;
 var start;
 var end;
+
+var lowerBoard = ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "backspace",
+    "tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]",
+    "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "\'", "enter",
+    ";", "\\", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/",
+    "space"
+];
+
+var capBoard = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "backspace",
+    "tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}",
+    "caps", "A", "S", "D", "F", "G", "H", "J", "K", "L", "\"", "enter",
+    ":", "|", "Z", "X", "C", "V", "B", "N", "M", "<", ">", "?",
+    "space"
+];
 
 try {
     currentStack = [document.getElementById("userText").value];
@@ -40,72 +29,16 @@ try {
 {
     currentStack = [""];
 }
-
-//called On load
 function startUp() {
-    makeBoard();
     getCursor();
+    //CHECK LOCAL STORAGE FOR ITENM OF LAST SESSION AND THEN FILL IN DATA
+
     document.addEventListener('keyup', function (event) {
         getCursor();
         if (focus === "userText") {
             updateStack();
         }
     });
-}
-//switch between caps/lower
-function makeBoard() {
-    var keyboard = [];
-    if (cap) {
-        keyboard = capBoard;
-    } else {
-        keyboard = lowerBoard;
-    }
-    //create buttons
-    $("div.keyboardRow1").html("");
-    $("div.keyboardRow1").trigger("create");
-    $("div.keyboardRow2").html("");
-    $("div.keyboardRow2").trigger("create");
-    $("div.keyboardRow3").html("");
-    $("div.keyboardRow3").trigger("create");
-    $("div.keyboardRow4").html("");
-    $("div.keyboardRow4").trigger("create");
-
-//create 4 rows of words 
-    //string being built to represent the button in HTML
-    var htmlString = "";
-    var leng = 12;
-    var index = 0;
-
-//create the 4 blocks of buttons
-    for (j = 0; j < 4; j++) {
-        htmlString = "<div data-role='controlgroup' data-type='horizontal'>";
-        //Last row has 10 buttons
-        if (j === 3) {
-            leng = 10;
-        }
-
-        var i = 0;
-        while (i < leng) {
-            var displayString = keyboard[index];
-            if(keyboard[index] === "\\'" || keyboard[index] === "\\\\"){
-                displayString = keyboard[index].substring(1,2);
-            }
-            htmlString = htmlString +
-                    "<a data-role=\"button\" onclick=\"addWord('"
-                    + keyboard[index] + "')\">" + displayString + "</a>";
-            i++;
-            index++;
-        }
-        //end current block of buttons (each row)
-        htmlString = htmlString + "</div>";
-
-        //get the name of the row to be edited in the html file
-        var classStr = "div.keyboardRow" + (j + 1);
-
-        //change the text of that line to be the block of buttons
-        $(classStr).append(htmlString);
-        $(classStr).trigger("create");
-    }
 }
 
 /** 
@@ -114,7 +47,7 @@ function makeBoard() {
  * @param {String} word The word to be added to the textarea 
  **/
 function addWord(word) {
-document.getElementById(focus).focus();
+    document.getElementById(focus).focus();
     //Add the word to the textarea
     var nextString = document.getElementById(focus).value;
     var first = nextString.substring(0, start);
@@ -144,15 +77,17 @@ function del() {
 
     if (start === end) {
         document.getElementById(focus).value =
-                curString.substring(0, start - 1) 
+                curString.substring(0, start - 1)
                 + curString.substring(end, curString.length);
-        start--;
-        end--;
+        if (start > 0) {
+            start--;
+            end--;
+        }
         document.getElementById(focus).setSelectionRange(start, end);
     } else {
 
         document.getElementById(focus).value =
-                curString.substring(0, start) 
+                curString.substring(0, start)
                 + curString.substring(end, curString.length);
         end = start;
         document.getElementById(focus).setSelectionRange(start, end);
@@ -179,9 +114,9 @@ function redo() {
     document.getElementById(focus).focus();
     if (currentStack.length > 1) {
         oldStack.push(currentStack.pop());
-        document.getElementById("userText").value = 
+        document.getElementById("userText").value =
                 currentStack[currentStack.length - 1];
-         getCursor();
+        getCursor();
     }
 }
 
@@ -189,22 +124,14 @@ function redo() {
 function updateStack() {
     var checkEqual = currentStack.pop();
     if (checkEqual === document.getElementById("userText").value) {
-        currentStack.push(checkEqual);
+        currentStack.push(checkEqual);  
     } else {
-
+        currentStack = [];
         oldStack.push(checkEqual);
+        while(oldStack.length > 15){
+            oldStack.shift();
+        }
         currentStack.push(document.getElementById("userText").value);
-    }
-}
-
-//Change the keyboard
-function caps() {
-    if (cap) {
-        cap = false;
-        makeBoard();
-    } else {
-        cap = true;
-        makeBoard();
     }
 }
 
@@ -216,9 +143,35 @@ function checkField(num) {
     } else if (num === 0) {
         focus = "title";
     }
-
     getCursor();
 }
+
+//Publish current session to database
+function publish() {
+    var title = document.getElementById("title").value;
+    var body = document.getElementById("userText").value;
+    var author = document.getElementById("author").value;
+    var time = Date.now();
+
+    title = encodeURIComponent(title);
+    body = encodeURIComponent(body);
+    author = encodeURIComponent(author);
+
+    if (author != 'Judi' && author != "Michael") {
+        return;
+    }
+
+    //this stops escape characters from breaking the POST
+    data = 'title=' + title + '&body=' + body + '&author=' + author + "&time=" + time;
+
+    console.log(data);
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('POST', '/create/new', true);
+    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhttp.send(data);
+    alert("success");
+}
+
 
 //Gets cursor position in the string
 function getCursor() {
@@ -227,28 +180,159 @@ function getCursor() {
     start = curs.selectionStart;
     end = curs.selectionEnd;
 }
-//Publish current session to database
-function publish() {
-    var title = document.getElementById("title").value;
-    var body = document.getElementById("userText").value;
-    var author = document.getElementById("author").value;
-    var time = Date.now();
 
-    title =  encodeURIComponent(title);
-    body =  encodeURIComponent(body);
-    author =  encodeURIComponent(author);
+const Keyboard = {
+    elements: {
+        main: null,
+        keysContainer: null,
+        keys: []
+    },
 
-    if (author != 'Judi' && author != "Michael") {
-      return;
+    eventHandlers: {
+        oninput: null,
+        onclose: null
+    },
+
+    properties: {
+        value: "",
+        capsLock: false
+    },
+
+    init() {
+        // Create main elements
+        this.elements.main = document.createElement("div");
+        this.elements.keysContainer = document.createElement("div");
+
+        // Setup main elements
+        this.elements.main.classList.add("keyboard", "keyboard--hidden");
+        this.elements.keysContainer.classList.add("keyboard__keys");
+        this.elements.keysContainer.appendChild(this._createKeys());
+
+        this.elements.keys = this.elements.keysContainer.querySelectorAll(".keyboard__key");
+
+        // Add to DOM
+        this.elements.main.appendChild(this.elements.keysContainer);
+        document.body.appendChild(this.elements.main);
+
+        // Automatically use keyboard for elements with .use-keyboard-input
+        document.querySelectorAll(".use-keyboard-input").forEach(element => {
+            element.addEventListener("focus", () => {
+                this.open(element.value, currentValue => {
+                    element.value = currentValue;
+                });
+            });
+        });
+    },
+
+    _createKeys() {
+        const fragment = document.createDocumentFragment();
+
+        // Creates HTML for an icon
+        const createIconHTML = (icon_name) => {
+            return `<i class="material-icons">${icon_name}</i>`;
+        };
+
+        lowerBoard.forEach(key => {
+            const keyElement = document.createElement("button");
+            const insertLineBreak = ["backspace", "]", "enter", "/"].indexOf(key) !== -1;
+
+            // Add attributes/classes
+            keyElement.setAttribute("type", "button");
+            keyElement.classList.add("keyboard__key");
+
+            switch (key) {
+                case "backspace":
+                    keyElement.classList.add("keyboard__key--wide");
+                    keyElement.innerHTML = createIconHTML("backspace");
+
+                    keyElement.addEventListener("click", () => {
+                        del();
+                    });
+
+                    break;
+
+                case "caps":
+                    keyElement.classList.add("keyboard__key--wide", "keyboard__key--activatable");
+                    keyElement.innerHTML = createIconHTML("keyboard_capslock");
+
+                    keyElement.addEventListener("click", () => {
+                        this._toggleCapsLock();
+                        keyElement.classList.toggle("keyboard__key--active", this.properties.capsLock);
+                    });
+
+                    break;
+
+                case "enter":
+                    keyElement.classList.add("keyboard__key--wide");
+                    keyElement.innerHTML = createIconHTML("keyboard_return");
+
+                    keyElement.addEventListener("click", () => {
+                        addWord("\n");
+                    });
+
+                    break;
+
+                case "tab":
+                    keyElement.classList.add("keyboard__key--wide");
+                    keyElement.innerHTML = createIconHTML("keyboard_tab");
+
+                    keyElement.addEventListener("click", () => {
+                        addWord("\t");
+                    });
+
+                    break;
+
+                case "space":
+                    keyElement.classList.add("keyboard__key--extra-wide");
+                    keyElement.innerHTML = createIconHTML("space_bar");
+
+                    keyElement.addEventListener("click", () => {
+                        addWord(" ");
+                    });
+
+                    break;
+
+                default:
+                    keyElement.textContent = key.toLowerCase();
+
+                    keyElement.addEventListener("click", () => {
+                        addWord(keyElement.textContent);
+                    });
+                    break;
+            }
+
+            fragment.appendChild(keyElement);
+
+            if (insertLineBreak) {
+                fragment.appendChild(document.createElement("br"));
+            }
+        });
+
+        return fragment;
+    },
+
+    _toggleCapsLock() {
+        this.properties.capsLock = !this.properties.capsLock;
+        var board;
+        if (this.properties.capsLock) {
+            board = capBoard;
+        } else {
+            board = lowerBoard;
+        }
+        var i = 0;
+        for (const key of this.elements.keys) {
+
+            if (key.childElementCount === 0) {
+                key.textContent = board[i];
+            }
+            i++;
+        }
     }
+};
 
-    //this stops escape characters from breaking the POST
-    data = 'title='+title+'&body='+body+'&author='+author+"&time="+time;
+window.addEventListener("DOMContentLoaded", function () {
+    Keyboard.init();
+});
 
-    console.log(data);
-    var xhttp = new XMLHttpRequest();
-    xhttp.open('POST', '/create/new', true);
-    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhttp.send(data);
-}
+
 
